650668bef85c6c9132731795f8ff9507a8e6f922
cliqr2015 <cliqr2015@gmail.com>
2020-03-18 12:21:05 -0700

    Hello Eg

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/egor', function (req, res) {
+  res.send('Hello Egor!');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
