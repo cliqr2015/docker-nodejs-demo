c7a283e615a8867bc2d4ed7b0454ac2c97bc1309
cliqr2015 <cliqr2015@gmail.com>
2020-03-19 17:56:53 -0700

    m19-01

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/green', function (req, res) {
+  res.send('This\'s Green color!');
+});
+
+app.get('/yellow', function (req, res) {
+  res.send('This\'s Yellow color!');
+});
+
+app.get('/egor', function (req, res) {
+  res.send('Hello Egor!');
+});
+
+app.get('/about', function (req, res) {
+  res.send('about')
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
